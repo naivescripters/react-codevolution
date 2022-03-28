a59976646f74908271c31e3c52ead1132527e213
Abul Hasan <abul.hasan.ict@gmail.com>
2022-03-28 12:08:18 +0600

    Conditional rendering

---
+++ src/components/UserGreeting.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+
+class UserGreeting extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+        isLoggedIn: false
+      }
+    }
+  render() {
+
+    return this.state.isLoggedIn && <div>Welcome Vishwas</div>
+
+    //   return this.state.isLoggedIn ? (
+    //     <div>Welcome Vishwas</div>
+    //   ) : (
+    //     <div>Welcome Guest</div>
+    //   )
+
+    // let message;
+
+    // if(this.state.isLoggedIn) {
+    //     message = 'Welcome Vishwas';
+    // } else {
+    //     message = 'Welcome Guest';
+    // }
+
+    // return (
+    //     <div>{message}</div>
+    // )
+
+    //   if(this.state.isLoggedIn) {
+    //     return (
+    //         <div>
+    //             Welcome Vishwas
+    //         </div>
+    //     )
+    //   } else {
+    //         return (
+    //             <div>
+    //                 Welcome Guest
+    //             </div>
+    //         )
+    //   }
+  }
+}
+
+export default UserGreeting
