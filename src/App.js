d3a71e38dd3d64d374f34d961267efd0876f44ec
Abul Hasan <abul.hasan.ict@gmail.com>
2022-03-30 14:31:59 +0600

    Mounting lifecycle method

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from './components/Form';
+import LifecycleA from './components/LifecycleA';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Form />
+        {/* <Form /> */}
+        <LifecycleA />
       </div>
     )
   }
---
+++ src/components/LifecycleB.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+class LifecycleB extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         name: 'Vishwas'
+      }
+
+      console.log('LifecycleB constructor')
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        console.log('LifecycleB getDerivedStateFormProps');
+        return null;
+    }
+
+    componentDidMount() {
+        console.log('LifecycleB componentDidMount');
+    }
+
+  render() {
+      console.log('LifecycleB render')
+    return (
+      <div>
+          Lifecycle B
+      </div>
+    )
+  }
+}
+
+export default LifecycleB
