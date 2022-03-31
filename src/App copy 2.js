4a7e7e1e3dba954811f84e324636659f260b77ae
Abul Hasan <abul.hasan.ict@gmail.com>
2022-03-31 23:43:04 +0600

    React fragment

--- src/App copy 2.js
+++ src/App copy 2.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import FragmentDemo from './components/FragmentDemo';
-import Table from './components/Table';
+import Form from './components/Form';
+import LifecycleA from './components/LifecycleA';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        {/* <FragmentDemo /> */}
-        <Table />
+        {/* <Form /> */}
+        <LifecycleA />
       </div>
     )
   }
