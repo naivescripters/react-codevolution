a5dca18957ad843c8ec740c5018b69486e8a1463
Abul Hasan <abul.hasan.ict@gmail.com>
2022-04-03 12:02:48 +0600

    Refs with class components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import ParentComp from './components/ParentComp';
+import FocusInput from './components/FocusInput';
+import RefsDemo from './components/RefsDemo';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <ParentComp />
+        <FocusInput />
+        {/* <RefsDemo /> */}
       </div>
     )
   }
