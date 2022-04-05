83a1c2b13766017c20a0be7aeddff16da3684dbb
Abul Hasan <abul.hasan.ict@gmail.com>
2022-04-05 10:17:10 +0600

    React forward ref

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import FocusInput from './components/FocusInput';
+import FRParentInput from './components/FRParentInput';
 import RefsDemo from './components/RefsDemo';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <FocusInput />
+        <FRParentInput />
+        {/* <FocusInput /> */}
         {/* <RefsDemo /> */}
       </div>
     )
